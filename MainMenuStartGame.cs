372d372da2152ab681f107ce9fc4cac92db33d70
YimaPhilemon <54995392+YimaPhilemon@users.noreply.github.com>
2021-12-08 18:12:06 +0100

    Update MainMenuStartGame.cs

--- MainMenuStartGame.cs
+++ MainMenuStartGame.cs
@@ -7,35 +7,22 @@ using UnityEngine.UI;
 public class MainMenuStartGame : MonoBehaviour
 {
     public static MainMenuStartGame Instance;
-	public GameObject button1;
-	public GameObject button2;
+	
 	public Text load;
 	public Slider slide;
 	float progressinfo;
 
 	// Start is called before the first frame update
-	void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	
 
     public void LoadSceneWithCharChoice(string levelName) {
 		//SceneManager.LoadScene(levelName);
 		StartCoroutine(LoadScene(levelName));
-		button1.SetActive(false);
-		button2.SetActive(false);
+		
 	}
 
-    public void RunInterstitialAd() {
-        //AdsManager.Instance.RemoteShowInterstitial();
-        //AdsManager.Instance.ShowAdmobInterstitial();
-    }
+    
+
 
 	IEnumerator LoadScene(string level)
 	{
