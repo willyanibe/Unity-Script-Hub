87d54eab3c8f53a1e90bc87221cb90e2071ddad3
Wilson Anibe Egwoba <68034083+willyanibe@users.noreply.github.com>
2021-12-13 22:12:09 +0100

    Billboard script for sprites on the Y axis alone
    This script helps position your sprites to always rotate towards the camera on the Y axis

---
+++ Billboard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Billboard : MonoBehaviour
+{
+    void Update() 
+    {
+
+    }
+
+     void LateUpdate() {
+        Quaternion r1 = Quaternion.LookRotation(transform.position - Camera.main.transform.position, Vector3.up);
+        Vector3 euler2 = transform.eulerAngles;
+        transform.rotation = Quaternion.Euler(r1.eulerAngles.x, euler2.y, euler2.z);
+    }
+}
